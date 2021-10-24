e3740644fedd6754ecca161b410cc8573de8c023
aymen <aymen.rahali@esprit.tn>
2021-10-24 12:23:47 +0100

    2nd commit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include<string>
-#include"header.h"
+
 
 
 using namespace std;
 
 int main()
-{ compte c1 ;
-float m,solde;
-
-c1.saisir();
-c1.affiche();
-   cout << "donenr le montant"  << endl;
-    cin >> m ;
-  /*  solde=c1.recharge(m);
-    cout<<"votre solde est : "<<solde<<endl;*/
-    c1.recharge(m);
+{ cout<<hii;
     return 0;
 }
